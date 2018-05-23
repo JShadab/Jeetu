5f67e7715d70091020423167dc3f803a98a19544
fidelix18 <shadab.khan.2628@gmail.com>
2018-05-22 17:25:55 -0700

    Jeetu Assibnment

--- Jeetu/src/main/java/db/DbConnection.java
+++ Jeetu/src/main/java/db/DbConnection.java
@@ -12,7 +12,7 @@ public class DbConnection {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/empDB", "root", "root");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/empDB", "root", "");
 
 		} catch (Exception e) {
 
--- Jeetu/src/main/java/servlet/InsertEmpServlet.java
+++ Jeetu/src/main/java/servlet/InsertEmpServlet.java
@@ -14,7 +14,7 @@ import db.EmpDAO;
 public class InsertEmpServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		// Getting values from HTMl page
 		String name = req.getParameter("name");
